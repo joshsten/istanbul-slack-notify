da821aa0f3bdd28adc6242979f76ed4c8c9dc6ba
Joshua Enfield <enfield@civicplus.com>
2018-03-08 16:47:15 -0600

    Fixed istanbul notifications

--- src/commit-info.js
+++ src/commit-info.js
@@ -32,13 +32,13 @@ class CommitInfo {
     static git() {
         return new Promise((resolve, reject) => {
             let format = JSON.stringify({
-                "shortRevision": "%h",
-                "revision": "%H",
-                "date": "%cr",
-                "subject": "%f",
-                "author": "%an",
-                "authorEmail": "%ae",
-                "refs": "%d"
+                "\"shortRevision\"": "\"%h\"",
+                "\"revision\"": "\"%H\"",
+                "\"date\"": "\"%cr\"",
+                "\"subject\"": "\"%f\"",
+                "\"author\"": "\"%an\"",
+                "\"authorEmail\"": "\"%ae\"",
+                "\"refs\"": "\"%d\""
             });
             let command = `git log -1 --no-color --decorate=short --pretty=format:'${format}' HEAD`;
             exec(command, (err, stdout, stderr) => {
